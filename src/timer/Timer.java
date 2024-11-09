ca2279a70036ce4155f728148c2cb3e92a19fb7c
ahmet <ahmett123@hotmail.com>
2024-11-10 00:16:04 +0300

    some changes

--- src/timer/Timer.java
+++ src/timer/Timer.java
@@ -6,21 +6,19 @@ public class Timer {
     public static void main(String[] args) {
 
 
-        int min ;
+        int min;
         int sec = 59;
 
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter minute for timer: ");
-        min= scanner.nextInt();
+        min = scanner.nextInt();
 
         while (min >= 0) {
             //for (int j = min; j >=0; j--) {
 
             for (int i = sec; i >= 0; i--) {
 
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
                 System.out.println(min + ":" + i);
                 try {
                     Thread.sleep(1000);
@@ -35,8 +33,6 @@ public class Timer {
             // System.out.println(min + ":" + sec);
         }
     }
-
-
 
 
 }
