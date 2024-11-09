9ea489cd4e7cd9d6ab90610707ef2463db5f4ea4
ahmet <ahmett123@hotmail.com>
2024-11-09 23:32:47 +0300

    First upload

---
+++ src/timer/Timer.java
@@ -0,0 +1,42 @@
+package timer;
+
+import java.util.Scanner;
+
+public class Timer {
+    public static void main(String[] args) {
+
+
+        int min = 0;
+        int sec = 59;
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.print("Enter minute for timer: ");
+        min= scanner.nextInt();
+
+        while (min >= 0) {
+            //for (int j = min; j >=0; j--) {
+
+            for (int i = sec; i >= 0; i--) {
+
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println(min + ":" + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+                if (i == 0) {
+                    min--;
+                }
+            }
+
+            // System.out.println(min + ":" + sec);
+        }
+    }
+
+
+
+
+}
